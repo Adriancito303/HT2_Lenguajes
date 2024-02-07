ea1c21e456af1b6b37240dfa62672744ea90799c
engelsruiz09 <jaruizc@correo.url.edu.gt>
2024-02-07 15:05:39 -0600

    regex letra con caso de prueba

--- PROYECTO-PARTE1/Program.cs
+++ PROYECTO-PARTE1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using Sets;
+
+string filePath = "C:\\Users\\julio\\Downloads\\GRAMATICA.txt";
+int lineNumber = 0;
+
+foreach (string line in File.ReadLines(filePath))
+{
+    lineNumber++; //Incrementa el contador de lineas
+
+    //ignoralas lineas vacias o que solo contienen espacios en blanco
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        continue; //Pasa a la siguiente línea
+    }
+    Letra.VerificaLetra(line, lineNumber);
+}
---
+++ PROYECTO-PARTE1/Sets/Letra.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Sets
+{
+    public class Letra
+    {
+        public static void VerificaLetra(string line, int num)
+        {
+            var pattern = @"^\s*([a-zA-Z_]+)\s*=\s*((('[a-zA-Z]')(\s*\.\.\s*('[a-zA-Z]'))?)|('[_]'))(\s*\+\s*((('[a-zA-Z]')(\s*\.\.\s*('[a-zA-Z]'))?)|('[_]')))*\s*$";
+
+            // \s* -> espacio en blanco
+            // * a lo mejor podria haber algo, y si hay algo podria ser infinito 
+            // \. busca una unica aparicion del separador decimal
+            // el operador de asignacion (=) permite espacios alrededor
+            // + por lo menos tiene que haber algo de lo que especifico , por lo menos una infinito
+            int lineNumber = num + 1;
+            if (Regex.IsMatch(line.Trim(), pattern))
+            {
+                Console.WriteLine("SET valido en la línea " + lineNumber);
+            }
+            else
+            {
+                Console.WriteLine("No es correcto, error en la línea: " + lineNumber);
+            }
+        }
+    }
+}
